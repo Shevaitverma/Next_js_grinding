adf80afe6a16ec9f81665f5fd2367f9c2525e05c
Shevaitverma <shevaitverma333@gmail.com>
2024-11-12 23:55:16 +0530

    logout functionality

--- proj_auth/src/app/profile/page.tsx
+++ proj_auth/src/app/profile/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 function profile() {
+
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("logout successful");
+      router.push("/login")
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  }
+
   return (
-    <div>profile</div>
+    <div className='flex flex-col items-center justify-center min-h-screen p-2'>
+        <h1 className='my-4'>Profile</h1>
+        <hr /> 
+        <button 
+          className='bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded'
+          onClick={logout}
+        >
+          Logout
+        </button>
+    </div>
   )
 }
 
